b8820daf0c01cee5cbefbb01f3c05264ea8736e0
graciaa-glitch <lotiegraciaa@gmail.com>
2025-11-11 15:20:58 +0100

    Delete vuln_hardcoded_secret.py

--- vuln_hardcoded_secret.py
+++
@@ -1,6 +0,0 @@
-# vuln_hardcoded_secret.py
-# Bandit détecte les secrets/mots de passe en dur (B105)
-# => Risque de divulgation d’informations sensibles
-
-PASSWORD = "SuperSecret123!"
-print("Connexion avec mot de passe :", PASSWORD)
