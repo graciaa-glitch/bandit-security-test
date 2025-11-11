76be6129af0e483618479aea861ae252edf7a601
graciaa-glitch <lotiegraciaa@gmail.com>
2025-11-11 15:22:37 +0100

    Delete vuln_eval.py

--- vuln_eval.py
+++
@@ -1,8 +0,0 @@
-# vuln_eval.py
-# Bandit détecte l'usage de eval() comme vulnérabilité HIGH (B307)
-# => Risque d'exécution de code arbitraire si l'entrée vient d'un utilisateur
-
-user_input = "2 + 2"
-# Simulation : entrée non sécurisée
-result = eval(user_input)
-print("Résultat :", result)
