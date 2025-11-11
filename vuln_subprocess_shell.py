5a39d4adf9fd2677e7d77e5a365d9c0d299ab701
graciaa-glitch <lotiegraciaa@gmail.com>
2025-11-11 15:21:46 +0100

    Delete vuln_subprocess_shell.py

--- vuln_subprocess_shell.py
+++
@@ -1,8 +0,0 @@
-# vuln_subprocess_shell.py
-# Bandit détecte subprocess avec shell=True comme vulnérabilité HIGH (B602, B607)
-# => Risque d'injection de commande
-
-import subprocess
-
-cmd = "ls -la"
-subprocess.run(cmd, shell=True)
