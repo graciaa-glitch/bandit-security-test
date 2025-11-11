8f4e922c0a3cea02bea1b92a18a313d93382765b
graciaa-glitch <lotiegraciaa@gmail.com>
2025-11-11 15:23:41 +0100

    Delete .github/workflows/vuln_subprocess_shell.py

--- .github/workflows/vuln_subprocess_shell.py
+++
@@ -1,7 +0,0 @@
-# vuln_subprocess_shell.py
-# Bandit signale subprocess.run(..., shell=True) comme HIGH
-import subprocess
-
-cmd = "ls -la"
-# usage de shell=True -> risque d'injection
-subprocess.run(cmd, shell=True)
